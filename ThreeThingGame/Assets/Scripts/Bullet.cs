85e8631c0827dac923a72db4f3f7405a3f2ba4e4
Kisielekw <dawkis1@outlook.com>
2022-11-28 11:02:53 +0000

    sound in bullets

--- ThreeThingGame/Assets/Scripts/Bullet.cs
+++ ThreeThingGame/Assets/Scripts/Bullet.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public AudioClip HitSound;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(this.gameObject);
+        GetComponent<AudioSource>().PlayOneShot(HitSound);
+        GetComponent<Transform>().position = new Vector3(1000,1000,100);
+        Destroy(this.gameObject, 0.5f);
     }
 }
